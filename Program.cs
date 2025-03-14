dfaafac1a42963e1f299c10b16080cf856204e97
Eros Klein <support@richmoney.life>
2025-03-14 11:37:45 +0100

    remove useless variable

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
-﻿using System.Runtime.CompilerServices;
-using EFCoreWriting;
+﻿using EFCoreWriting;
 using Microsoft.EntityFrameworkCore;
 
 
 var factory = new DataContextFactory();
-
 var context = factory.CreateDbContext(args: []);
 
 await context.OrderHeaders.ExecuteDeleteAsync();
 await context.OrderLines.ExecuteDeleteAsync();
 await context.Customers.ExecuteDeleteAsync();
 
-var customers = await Parser.Parse(File.ReadAllLines("data/data.txt"), context);
+await Parser.Parse(File.ReadAllLines("data/data.txt"), context);
--- obj/Debug/net9.0/c#-sue.AssemblyInfo.cs
+++ obj/Debug/net9.0/c#-sue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#-sue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b37e13d7b6eeaf79a60bf326dfd9ac2ca6ca2bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca715d48aaae976b172a973898e5ede35122fb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#-sue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#-sue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
